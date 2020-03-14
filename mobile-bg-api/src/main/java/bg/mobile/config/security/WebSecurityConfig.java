d8ed3594dc0264bfa25c18dacda5f4d69d6a534b
Bisu-bg <stefan.vadev@gmail.com>
2020-03-14 20:51:52 +0200

    tweaks for FE

--- mobile-bg-api/src/main/java/bg/mobile/config/security/WebSecurityConfig.java
+++ mobile-bg-api/src/main/java/bg/mobile/config/security/WebSecurityConfig.java
@@ -12,7 +12,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
   @Override
   protected void configure(final HttpSecurity http) throws Exception {
-    http.csrf().disable().authorizeRequests()
+    http.cors().and().csrf().disable().authorizeRequests()
         .anyRequest().authenticated()
         .and()
         .addFilter(new JWTAuthorizationFilter(authenticationManager()))
