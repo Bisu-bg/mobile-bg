8525703fb5417ecf0f8eccb8d2296f91db410695
Bisu-bg <stefan.vadev@gmail.com>
2020-03-14 20:46:01 +0200

    tweaks for FE

--- mobile-bg-api/src/main/java/bg/mobile/config/security/WebSecurityConfig.java
+++ mobile-bg-api/src/main/java/bg/mobile/config/security/WebSecurityConfig.java
@@ -1,15 +1,11 @@
 package bg.mobile.config.security;
 
 import bg.mobile.config.security.jwt.JWTAuthorizationFilter;
-import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.CorsConfigurationSource;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 @EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
@@ -36,13 +32,6 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
         "/swagger-resources/**",
         "/swagger-ui.html",
         "/webjars/**");
-  }
-
-  @Bean
-  public CorsConfigurationSource corsConfigurationSource() {
-    final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-    source.registerCorsConfiguration("/**", new CorsConfiguration().applyPermitDefaultValues());
-    return source;
   }
 
 }
