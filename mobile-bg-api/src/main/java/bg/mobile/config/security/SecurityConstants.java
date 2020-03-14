23df5b49f800663ceca71160e3b184e92198f538
Bisu-bg <stefan.vadev@gmail.com>
2020-03-14 20:59:39 +0200

    tweaks for FE

--- mobile-bg-api/src/main/java/bg/mobile/config/security/SecurityConstants.java
+++ mobile-bg-api/src/main/java/bg/mobile/config/security/SecurityConstants.java
@@ -6,6 +6,6 @@ public class SecurityConstants {
   public static final long EXPIRATION_TIME = 86400000; // 1 day
   public static final String TOKEN_PREFIX = "Bearer ";
   public static final String HEADER_STRING = "Authorization";
-  public static final String REGISTER_URL = "/register";
-  public static final String LOGIN_URL = "/login";
+  public static final String REGISTER_URL = "/users/register";
+  public static final String LOGIN_URL = "/users/login";
 }
--- mobile-bg-api/src/main/java/bg/mobile/config/security/WebSecurityConfig.java
+++ mobile-bg-api/src/main/java/bg/mobile/config/security/WebSecurityConfig.java
@@ -4,12 +4,16 @@ import static bg.mobile.config.security.SecurityConstants.LOGIN_URL;
 import static bg.mobile.config.security.SecurityConstants.REGISTER_URL;
 
 import bg.mobile.config.security.jwt.JWTAuthorizationFilter;
+import org.springframework.context.annotation.Bean;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 @EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
@@ -35,6 +39,13 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
         "/swagger-resources/**",
         "/swagger-ui.html",
         "/webjars/**");
+  }
+
+  @Bean
+  public CorsConfigurationSource corsConfigurationSource() {
+    final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+    source.registerCorsConfiguration("/**", new CorsConfiguration().applyPermitDefaultValues());
+    return source;
   }
 
 }
