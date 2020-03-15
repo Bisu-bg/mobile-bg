0693db8fb5efa6094d0bce1a153ecff2093faf57
Bisu-bg <stefan.vadev@gmail.com>
2020-03-15 17:44:28 +0200

    test

--- mobile-bg-api/src/main/java/bg/mobile/config/security/WebSecurityConfig.java
+++ mobile-bg-api/src/main/java/bg/mobile/config/security/WebSecurityConfig.java
@@ -4,24 +4,21 @@ import static bg.mobile.config.security.SecurityConstants.CARS_URL;
 import static bg.mobile.config.security.SecurityConstants.USERS_URL;
 
 import bg.mobile.config.security.jwt.JWTAuthorizationFilter;
-import org.springframework.context.annotation.Bean;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.CorsConfigurationSource;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 @EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
   @Override
   protected void configure(final HttpSecurity http) throws Exception {
-    http.cors().and().csrf().disable().authorizeRequests()
-        .antMatchers(HttpMethod.POST, USERS_URL).permitAll()
+    http.cors().and().csrf().disable();
+    http.authorizeRequests()
+        .antMatchers(HttpMethod.POST, USERS_URL, CARS_URL).permitAll()
         .anyRequest().authenticated()
         .and()
         .addFilter(new JWTAuthorizationFilter(authenticationManager()))
@@ -31,7 +28,6 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
   @Override
   public void configure(final WebSecurity web) {
     web.ignoring().antMatchers(
-        "/cars/**",
         "/configuration/ui",
         "/configuration/**",
         "/actuator/**",
@@ -39,13 +35,6 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
         "/swagger-resources/**",
         "/swagger-ui.html",
         "/webjars/**");
-  }
-
-  @Bean
-  public CorsConfigurationSource corsConfigurationSource() {
-    final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-    source.registerCorsConfiguration("/**", new CorsConfiguration().applyPermitDefaultValues());
-    return source;
   }
 
 }
