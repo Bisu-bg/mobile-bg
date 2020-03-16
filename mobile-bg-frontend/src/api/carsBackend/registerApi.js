5fcacb8dd16de7185869c474f3eecee47293f9c4
miliev <miliev@lely.com>
2020-03-16 18:12:47 +0200

    Fixed register defect

--- mobile-bg-frontend/src/api/carsBackend/registerApi.js
+++ mobile-bg-frontend/src/api/carsBackend/registerApi.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 import { REGISTER_SERVICE_URL } from './config';
 
 export const api = {
-  register: async function(firstName,lastName,password,username) {
+  register: async function(username,password,firstName,lastName) {
     return axios.post(REGISTER_SERVICE_URL, {
       firstName,
       lastName,
