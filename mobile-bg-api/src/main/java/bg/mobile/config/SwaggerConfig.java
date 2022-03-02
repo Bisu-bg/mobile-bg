9512742d0d9c4f8efbc6b47a1e13f7f909861b96
Borislav Milev <lallushe13@gmail.com>
2022-03-02 12:18:41 +0200

    swagger fix

--- mobile-bg-api/src/main/java/bg/mobile/config/SwaggerConfig.java
+++ mobile-bg-api/src/main/java/bg/mobile/config/SwaggerConfig.java
@@ -1,23 +1,41 @@
 package bg.mobile.config;
 
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springdoc.core.GroupedOpenApi;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import springfox.documentation.builders.PathSelectors;
-import springfox.documentation.builders.RequestHandlerSelectors;
-import springfox.documentation.spi.DocumentationType;
-import springfox.documentation.spring.web.plugins.Docket;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @Configuration
-@EnableSwagger2
 public class SwaggerConfig {
 
   @Bean
-  public Docket api() {
-    return new Docket(DocumentationType.SWAGGER_2)
-        .select()
-        .apis(RequestHandlerSelectors.any())
-        .paths(PathSelectors.any())
-        .build();
+  public GroupedOpenApi apiGroup() {
+    return GroupedOpenApi
+            .builder()
+            .group("API")
+            .pathsToMatch("/**")
+            .build();
+  }
+
+  @Bean
+  public OpenAPI apiInfo() {
+    final String securitySchemeName = "bearerAuth";
+    return new OpenAPI()
+            .addSecurityItem(new SecurityRequirement().addList(securitySchemeName))
+            .components(new Components().addSecuritySchemes(
+                    securitySchemeName,
+                    new SecurityScheme()
+                            .name(securitySchemeName)
+                            .type(SecurityScheme.Type.HTTP)
+                            .scheme("bearer")
+                            .bearerFormat("JWT")))
+            .info(new Info()
+                    .title("Mobile API")
+                    .description("Rest API for Mobile")
+                    .version("1.0"));
   }
 }
