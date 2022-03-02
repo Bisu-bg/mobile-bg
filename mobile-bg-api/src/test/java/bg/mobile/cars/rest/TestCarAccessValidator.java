dd1ac314d4542358466aa94a17999169f2867415
Borislav Milev <lallushe13@gmail.com>
2022-03-02 12:17:03 +0200

    reformatting tests

--- mobile-bg-api/src/test/java/bg/mobile/cars/rest/TestCarAccessValidator.java
+++ mobile-bg-api/src/test/java/bg/mobile/cars/rest/TestCarAccessValidator.java
@@ -1,38 +1,13 @@
 package bg.mobile.cars.rest;
 
-import static bg.mobile.cars.entities.Condition.USED;
-import static bg.mobile.cars.entities.EngineType.GASOLINE;
-import static bg.mobile.cars.entities.GearBox.AUTOMATIC;
+import bg.mobile.base.BaseTest;
+import bg.mobile.cars.models.CarModel;
+import bg.mobile.exceptions.HttpForbiddenException;
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
-import bg.mobile.cars.models.CarModel;
-import bg.mobile.cars.service.CarService;
-import bg.mobile.exceptions.HttpForbiddenException;
-import bg.mobile.users.model.UserModel;
-import bg.mobile.users.service.UserService;
-import java.math.BigDecimal;
-import java.util.Collections;
-import java.util.HashSet;
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ActiveProfiles;
-import org.springframework.test.context.junit4.SpringRunner;
-
-@RunWith(SpringRunner.class)
-@SpringBootTest
-@ActiveProfiles("test")
-public class TestCarAccessValidator {
-
-  @Autowired
-  private UserService userService;
-
-  @Autowired
-  private CarAccessValidator carAccessValidator;
-
-  @Autowired
-  private CarService carService;
+public class TestCarAccessValidator extends BaseTest {
 
   @Test
   public void testCarAccessValidator() {
@@ -46,26 +21,4 @@ public class TestCarAccessValidator {
         HttpForbiddenException.class,
         () -> carAccessValidator.validateUserCanEditCar("bad id", created.getId()));
   }
-
-  private CarModel buildCar() {
-    final UserModel user = new UserModel(null, "Kircata", "1234", "Kiril", "Petkov");
-    final UserModel createdUser = userService.registerUser(user);
-
-    final CarModel car = new CarModel();
-    car.setMake("Mazda");
-    car.setModel("3");
-    car.setYear(2004);
-    car.setEngineType(GASOLINE);
-    car.setGearBox(AUTOMATIC);
-    car.setCondition(USED);
-    car.setHorsePower(150);
-    car.setColor("Black");
-    car.setPrice(new BigDecimal(12000));
-    car.setCity("Sofia");
-    car.setMileage(150000);
-    car.setUser(createdUser);
-    car.setExtras("Leather Interior");
-    return car;
-  }
-
 }
--- mobile-bg-api/src/test/java/bg/mobile/cars/service/TestCarService.java
+++ mobile-bg-api/src/test/java/bg/mobile/cars/service/TestCarService.java
@@ -1,37 +1,14 @@
 package bg.mobile.cars.service;
 
-import static bg.mobile.cars.entities.Condition.USED;
-import static bg.mobile.cars.entities.EngineType.GASOLINE;
-import static bg.mobile.cars.entities.GearBox.AUTOMATIC;
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNotNull;
-import static org.junit.Assert.assertNull;
-import static org.junit.Assert.assertTrue;
-
+import bg.mobile.base.BaseTest;
 import bg.mobile.cars.models.CarModel;
-import bg.mobile.users.model.UserModel;
-import bg.mobile.users.service.UserService;
-import java.math.BigDecimal;
-import java.util.Collections;
-import java.util.HashSet;
+import org.junit.jupiter.api.Test;
+
 import java.util.List;
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ActiveProfiles;
-import org.springframework.test.context.junit4.SpringRunner;
 
-@RunWith(SpringRunner.class)
-@SpringBootTest
-@ActiveProfiles("test")
-public class TestCarService {
+import static org.junit.jupiter.api.Assertions.*;
 
-  @Autowired
-  private CarService carService;
-
-  @Autowired
-  private UserService userService;
+public class TestCarService extends BaseTest {
 
   @Test
   public void testCRUDCar() {
@@ -68,26 +45,4 @@ public class TestCarService {
     final CarModel deleted = carService.getById(updated.getId());
     assertNull(deleted);
   }
-
-  private CarModel buildCar() {
-    final UserModel user = new UserModel(null, "kiro", "1234", "Kiril", "Petkov");
-    final UserModel createdUser = userService.registerUser(user);
-
-    final CarModel car = new CarModel();
-    car.setMake("Mazda");
-    car.setModel("3");
-    car.setYear(2004);
-    car.setEngineType(GASOLINE);
-    car.setGearBox(AUTOMATIC);
-    car.setCondition(USED);
-    car.setHorsePower(150);
-    car.setColor("Black");
-    car.setPrice(new BigDecimal(12000));
-    car.setCity("Sofia");
-    car.setMileage(150000);
-    car.setUser(createdUser);
-    car.setExtras("Music,Leather Interior");
-    return car;
-  }
-
 }
